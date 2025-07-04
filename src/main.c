ef67a15ab0f0be397adc59a42e6ec5d8669813d8
ndiaga <169577450+Nicknamely@users.noreply.github.com>
2025-07-04 04:09:59 +0000

    reafactor: Add cmake subdir

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include "./etudiant/etudiant.h"
 #include "./matiere/matiere.h"
 #include "./note/note.h"
-#include <stdio.h>
+/* #include <stdio.h> */
 #include <stdlib.h>
 
 int main() { return EXIT_SUCCESS; }
