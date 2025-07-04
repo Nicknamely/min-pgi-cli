abd1a0e331e387ca3e5e6c86ebbc93851e06aded
ndiaga <169577450+Nicknamely@users.noreply.github.com>
2025-07-04 03:30:35 +0000

    refactor: Change project sructure;
    This project structure is more organized and easy to navigate

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include "./classe/classe.h"
+#include "./etudiant/etudiant.h"
+#include "./matiere/matiere.h"
+#include "./note/note.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() { return EXIT_SUCCESS; }
