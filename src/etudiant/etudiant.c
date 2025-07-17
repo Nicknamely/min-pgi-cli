35e8814a8683292b3ad144d2a59a995f177d15e2
ndiaga <169577450+Nicknamely@users.noreply.github.com>
2025-07-17 12:07:19 +0000

    feat: create dynamic array for etudiant entities

--- src/etudiant/etudiant.c
+++ src/etudiant/etudiant.c
@@ -1,1 +1,38 @@
-#include "../include/etudiant.h"
+#include "./etudiant.h"
+#include <stdio.h>
+
+void initialiser_EtudiantDB(EtudiantDB *db, size_t capacite_initial) {
+  db->etudiants = malloc(capacite_initial * sizeof(Etudiant));
+  if (db->etudiants == NULL) {
+    fprintf(stderr, "Allocation de memoire echouer\n");
+    exit(EXIT_FAILURE);
+  }
+  db->taille = 0;
+}
+
+void ajouter_etudiant(EtudiantDB *db, Etudiant nouvel_etudiant) {
+  if (db->taille == db->capacite) {
+    size_t nouvelle_capacite = db->capacite * 2;
+
+    Etudiant *temp =
+        realloc(db->etudiants, nouvelle_capacite * sizeof(Etudiant));
+    if (temp == NULL) {
+      fprintf(stderr, "Reallocation de memoire echouer ! \n");
+      free(temp);
+    }
+    db->etudiants = temp;
+    db->capacite = nouvelle_capacite;
+  }
+  db->etudiants[db->taille++] = nouvel_etudiant;
+}
+
+void supprimer_etudiant(EtudiantDB *db, size_t index) {
+  if (index >= db->taille) {
+    fprintf(stderr,
+            "Index invalide, doit etre inferieur aux nombre d'etudiants");
+    return;
+  }
+  for (size_t i = index; index < db->taille - 1; i++) {
+    db->etudiants[i] = db->etudiants[i + 1];
+  }
+}
---
+++ src/etudiant/etudiant.h
@@ -0,0 +1,33 @@
+#ifndef ETUDIANT_H
+
+#define ETUDIANT_H
+
+#include <stdlib.h>
+// type date
+typedef struct Date {
+  int jour;
+  int mois;
+  int annee;
+} Date;
+
+// type etudiant
+typedef struct {
+  int numero;
+  char nom[30];
+  char prenom[20];
+  char email[20];
+  Date date_naissance;
+} Etudiant;
+
+// Tableau dynamique
+typedef struct {
+  Etudiant *etudiants;
+  size_t capacite;
+  size_t taille; // nombre d'element courrant dans le tableau
+} EtudiantDB;
+
+// Fonctions CRUD
+void initialiser_EtudiantDB(EtudiantDB *db, size_t capacite_initial);
+void ajouter_etudiant(EtudiantDB *db, Etudiant nouvel_etudiant);
+void supprimer_etudiant(EtudiantDB *db, size_t index);
+#endif // !ETUDIANT_H
