b361fa615d8ddf5953c86165bb8441cb553610f0
ndiaga <169577450+Nicknamely@users.noreply.github.com>
2025-07-17 12:10:21 +0000

    chore: add boilerplate

---
+++ src/utils/fn_helper.h
@@ -0,0 +1,12 @@
+#ifndef FN_HELPER_H
+#define FN_HELPER_H
+// TODO:
+// - valider un nombre
+// - valider un email
+// - valider un nom
+// - valider un prenom
+// - valider une date
+// - valider une moyenne
+//
+
+#endif
