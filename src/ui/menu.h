ed0cd70a8847730eadcc498bb022edb1e6e90a4f
ndiaga <169577450+Nicknamely@users.noreply.github.com>
2025-07-18 19:08:51 +0000

    refac: deplacer les fonction a menu dans menu

--- src/ui/menu.h
+++ src/ui/menu.h
@@ -4,6 +4,7 @@
 #include "../classe/classe_matiere.h"
 #include "../classe/classe.h"
 #include "../matiere/matiere.h"
+#include "../note/note.h"
 
 void menu_principal();
 void menu_classes();
@@ -11,5 +12,10 @@ void menu_matieres();
 void menu_etudiants();
 void menu_notes();
 void menu_classe_matiere();
+
+// Fonctions interactives pour le menu notes
+void supprimer_note_interactive(NoteDB *db_note, const char *chemin_notes);
+void modifier_note_interactive(NoteDB *db_note, const char *chemin_notes);
+void importer_notes_interactive(NoteDB *db_note, const char *chemin_notes, const EtudiantDB *db_etudiant, const MatiereDB *db_matiere);
 
 #endif // UI_MENU_H
