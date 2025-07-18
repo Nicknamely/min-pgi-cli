dbfdce727828724039fa92fe2efc0fdd58488aea
ndiaga <169577450+Nicknamely@users.noreply.github.com>
2025-07-18 09:58:43 +0000

    chore: refac

--- src/utils/init.h
+++ src/utils/init.h
@@ -3,6 +3,6 @@
 
 int creer_repertoire_session(char *nom_session, char *chemin_complet);
 void creer_csv_session(const char *chemin_classes, const char *chemin_matieres,
-                       const char *chemin_etudiants, const char *chemin_notes);
+                       const char *chemin_etudiants, const char *chemin_notes, const char *chemin_associations);
 
 #endif // !INIT_H
